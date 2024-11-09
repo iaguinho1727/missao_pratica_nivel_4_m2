c88e9074cd2dabbfd912c9bb65b3c48c5c0afb5b
postassiums <batataprogramadora@gmail.com>
2024-11-08 21:50:59 -0300

    :bug: fix: Removed livro linha becausa it was bugging layout

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import 'jquery';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'jquery';
+// import 'bootstrap/dist/js/bootstrap.min.js';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
